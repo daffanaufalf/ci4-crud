525588c12709ec1f0fd9eeaf2112ea3c43b56819
Daffa Naufal F <daffanfproject@gmail.com>
2023-05-26 13:59:07 +0700

    first commit

---
+++ app/Controllers/Budiluhur.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+
+class Budiluhur extends BaseController
+{
+    public function method_fti()
+    {
+        echo "Ini method FTI";
+    }
+    public function method_fikom()
+    {
+        echo "Ini method FIKOM";
+    }
+}
